dc635a2121c4c94ea3514ac15f15a7ba6da23dba
Levy Barbosa <marcio.levy20@gmail.com>
2022-04-05 23:20:06 -0300

    implemented round results screen

--- src/components/room/round/ResultsStep.svelte
+++ src/components/room/round/ResultsStep.svelte
@@ -8,16 +8,24 @@
         Label
     } from '@smui/list';
 
-
     import { _ } from 'svelte-i18n';
 
     import PlayerIcon from '@/components/misc/PlayerIcon.svelte';    
+    
+    import { room } from '@/stores';
     import type { Results, Round } from '@/typings/state';
     
     export let round: Round;
+    let results: Results;
+    $: results = round.results!;
 
-    let ordered: Results;
-    $: ordered = round.results!.sort((a, b) => b.score - a.score);
+    let remainingTime = $room.nextRoundStartsIn!;
+    const interval = setInterval(() => {
+        remainingTime -= 1;
+        if (remainingTime <= 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
 
     function getMedalColor(idx: number) {
         return [
@@ -31,14 +39,14 @@
 
 <Card variant="outlined" class="h-100 p-3 overflow-hidden d-flex flex-column justify-content-between text-center">
     <div>
-        <h4 class="mt-4 mb-1">{ $_('round.results') }</h4>
+        <h4 class="mt-4 mb-1">{ $_('round.results.title') }</h4>
     </div>
-    <div class="overflow-y-auto">
+    <div class="overflow-y-auto list-wrapper">
         <List
             avatarList
             nonInteractive
         >
-            {#each ordered as resultEntry, idx}
+            {#each results as resultEntry, idx}
                 <Item>
                     <Graphic>
                         <PlayerIcon player={resultEntry.player} size={32} />
@@ -62,8 +70,18 @@
         </List>
     </div>
     <div>
+        {#if round.isLastRound}
+            {$_('round.results.finalResultsIn', { values: { time: remainingTime } })}
+        {:else}
+            {$_('round.results.nextRoundIn', { values: { time: remainingTime } })}
+        {/if}
     </div>
 </Card>
 
 <style lang="scss">
+    .list-wrapper {
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+    }
 </style>
