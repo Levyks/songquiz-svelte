9bd6b281ab468bad0583fecd5a47ca442d0dd0f6
Levy Barbosa <marcio.levy20@gmail.com>
2022-04-21 14:26:59 -0300

    feat: added spanish i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
 register('en', () => import('@/locales/en.yaml'));
 register('pt-BR', () => import('@/locales/pt-BR.yaml'));
+register('es', () => import('@/locales/es.yaml'));
 
 init({
     fallbackLocale: 'en',
